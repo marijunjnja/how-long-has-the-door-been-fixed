b99ac970ace6e98770e7316324ffbd50756cfe95
Bannon Tanner <bannon.nicholas.tanner@ibm.com>
2019-11-15 10:07:47 -0600

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import moment from 'moment'
+import styled from 'styled-components'
 
-function App() {
+const App = props => {
+  const whenFixed = moment('2019-11-14T15:00:00')
+  const now = moment()
+
+  const Wrapper = styled.section`
+    padding: 4em;
+    background: papayawhip;
+  `
+  const Title = styled.h1`
+    font-size: 2em;
+    text-align: center;
+    color: mediumpurple;
+  `
+  const DateLine = styled.h2`
+  font-size: 2em;
+    color: mediumspringgreen;
+    text-align: center;
+  `
+  const Line = styled.h3`
+    text-align: center;
+    color: palevioletred;
+    font-size: 1em;
+  `
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Wrapper>
+      <Title>When was the door fixed</Title>
+      <DateLine>{whenFixed.format("dddd, MMMM Do YYYY, h:mm:ss a")}</DateLine>
+      <Line>{now.diff(whenFixed)} milliseconds</Line>
+      <Line>{now.diff(whenFixed, 'seconds', true)} seconds</Line>
+      <Line>{now.diff(whenFixed, 'minutes', true)} minutes</Line>
+      <Line>{now.diff(whenFixed, 'hours', true)} hours</Line>
+      <Line>{now.diff(whenFixed, 'days', true)} days</Line>
+      <Line>{now.diff(whenFixed, 'weeks', true)} weeks</Line>
+      <Line>{now.diff(whenFixed, 'months', true)} months</Line>
+      <Line>{now.diff(whenFixed, 'years', true)} years</Line>
+    </Wrapper>
+  )
 }
 
-export default App;
+export default App
